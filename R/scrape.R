be5964c4a2f3c08d8e886008a1d04f07ca103347
Earo Wang <earo.wang@gmail.com>
2017-08-01 07:32:19 +0000

    version 0.1.0

---
+++ R/scrape.R
@@ -0,0 +1,76 @@
+globalVariables(c("Time", "Count", "Sensor", "Date", "Date_Time"))
+#' API to Melbourne pedestrian data using R
+#'
+#' @param from Starting date.
+#' @param to Ending date.
+#' @param tz Time zone.
+#'
+#' @details The data is sourced from [Melbourne Open Data Portal](https://data.melbourne.vic.gov.au/Transport-Movement/Pedestrian-volume-updated-monthly-/b2ak-trbp).
+#'   At its heart, this function scrapes the data through the
+#'   "https://compedapi.herokuapp.com" api. A progress bar shows the download
+#'   status. Please refer to Melbourne Open Data Portal for more details about
+#'   the dataset and its policy.
+#' @return A data frame including "Sensor", "Date_Time", "Date", "Time",
+#'   "Count" variables.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   ped_df <- walk_melb()
+#'   head(ped_df)
+#' }
+walk_melb <- function(from = to - 6L, to = Sys.Date() - 1L, tz = "") {
+  stopifnot(class(from) == "Date" && class(to) == "Date")
+  stopifnot(from > as.Date("2009-05-31"))
+  stopifnot(from <= to)
+  yesterday <- Sys.Date() - 1L
+  if (to > yesterday) {
+    warning(
+      paste0("The data is only avaiable up to ", yesterday, "."),
+      call. = FALSE
+    )
+    to <- yesterday
+  }
+
+  date_range <- seq.Date(from = from, to = to, by = 1L)
+  prefix_url <- "https://compedapi.herokuapp.com/api/bydatecsv/"
+
+  fmt_date <- format(date_range, "%d-%m-%Y")
+  urls <- paste0(prefix_url, fmt_date)
+  p <- dplyr::progress_estimated(length(urls))
+  lst_dat <- lapply(urls, function(x) {
+    dat <- utils::read.csv(
+      x, skip = 8, nrows = 43, colClasses = c("character", rep("integer", 24)),
+      na.strings = "N/A", stringsAsFactors = FALSE, check.names = FALSE
+    )
+    p$tick()$print()
+    dat
+  })
+  lst_dat_x <- mapply(
+    function(x, y) dplyr::mutate(x, Date = y),
+    lst_dat, date_range,
+    SIMPLIFY = FALSE, USE.NAMES = FALSE
+  )
+  df_dat <- dplyr::bind_rows(lapply(lst_dat_x, function(x)
+    tidyr::gather(x, Time, Count, -c(Sensor, Date))
+  ))
+  df_dat <- dplyr::mutate(df_dat, Time = interp_time(Time))
+  df_dat <- dplyr::mutate(
+    df_dat,
+    Date_Time = as.POSIXct(paste(
+      Date, paste0(formatC(Time, width = 2, flag = "0"), ":00:00")), tz = tz
+    )
+  )
+  df_dat[, c("Sensor", "Date_Time", "Date", "Time", "Count")]
+}
+
+interp_time <- function(x) {
+  output <- integer(length = length(x))
+  morning <- grepl("am", x)
+  arvo <- grepl("pm", x)
+  num <- as.integer(gsub("[^0-9]", "", x))
+  output[morning] <- num[morning]
+  output[arvo] <- num[arvo] + 12L
+  output[x %in% "Noon"] <- 12L
+  output
+}
---
+++ README.md
@@ -0,0 +1,56 @@
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+[![Travis-CI Build Status](https://travis-ci.org/earowang/rwalkr.svg?branch=master)](https://travis-ci.org/earowang/rwalkr) [![CRAN\_Status\_Badge](http://www.r-pkg.org/badges/version/rwalkr)](https://cran.r-project.org/package=rwalkr)
+
+rwalkr
+======
+
+The goal of **rwalkr** is to provide API to the pedestrian data from the City of Melbourne in tidy data form.
+
+Installation
+------------
+
+You could install the stable version from CRAN:
+
+``` r
+install.packages("rwalkr")
+```
+
+You could install the development version from Github using:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("earowang/rwalkr")
+```
+
+Usage
+-----
+
+Currently there is only one function in the package, `walk_melb()`. The starting and ending dates inform which period to be scraped.
+
+``` r
+library(rwalkr)
+start_date <- as.Date("2017-07-01")
+end_date <- start_date + 6L
+ped <- walk_melb(from = start_date, to = end_date)
+head(ped)
+#>                  Sensor  Date_Time       Date Time Count
+#> 1         State Library 2017-07-01 2017-07-01    0   334
+#> 2 Collins Place (South) 2017-07-01 2017-07-01    0    82
+#> 3 Collins Place (North) 2017-07-01 2017-07-01    0    51
+#> 4     Flagstaff Station 2017-07-01 2017-07-01    0     0
+#> 5     Melbourne Central 2017-07-01 2017-07-01    0   826
+#> 6      Town Hall (West) 2017-07-01 2017-07-01    0   682
+```
+
+``` r
+library(ggplot2)
+ggplot(data = subset(ped, Sensor == "Melbourne Central")) +
+  geom_line(aes(x = Date_Time, y = Count))
+```
+
+![](man/figure/plot-1.png)
+
+Acknowledgements
+----------------
+
+Thanks to Stuart Lee (@sa-lee) for inspiring this package.
